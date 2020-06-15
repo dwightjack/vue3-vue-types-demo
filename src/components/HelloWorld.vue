3e5dc34177f0c0370b95b35b4b97a01e0b5cc7ed
dwightjack <marco.solazzi@gmail.com>
2020-06-15 17:40:55 +0900

    setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
+      For a guide and recipes on how to configure / customize this project,
+      <br />check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
@@ -95,12 +95,13 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { defineComponent } from "vue";
+import { string } from "vue-types";
 
-export default Vue.extend({
+export default defineComponent({
   name: "HelloWorld",
   props: {
-    msg: String
+    msg: string()
   }
 });
 </script>
